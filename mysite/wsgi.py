760f4a05c5c9fabc6088773e9bd57c26b5191f00
Micah <mayoms@gmail.com>
2015-06-03 18:35:26 -0700

    broke newrelic?

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -7,19 +7,16 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
-import os
 import newrelic.agent
-
 newrelic.agent.initialize()
+
+import os
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
-
 application = get_wsgi_application()
-
-application = newrelic.agent.WSGIApplicationWrapper(application)
 
 from whitenoise.django import DjangoWhiteNoise
 application = DjangoWhiteNoise(application)
